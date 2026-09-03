54355fa50ba3bc04e31f757b7737777cbd05cac1
rorodad-cyber <hyukjulee@gmail.com>
2026-09-04 08:29:10 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,12 +7,13 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import requests
 import streamlit as st
+import koreanize_matplotlib # 📌 폰트 깨짐 방지 마법 패키지 추가
 
 warnings.filterwarnings('ignore')
 
-# 폰트 설정
-plt.rcParams['font.family'] = 'Malgun Gothic'
+# 폰트 설정 (이 부분 수정)
 plt.rcParams['axes.unicode_minus'] = False
+
 
 # ==========================================
 # 1. 데이터 크롤링 함수들 (자동 인코딩 감지 적용)
